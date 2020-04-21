015d5f992af3a459ffe3f212e7f01ceaef7bb951
Apriliaaptr <apriliaaputri14@gmail.com>
2020-04-21 13:31:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ariesgurl/ui/BlankActivity.java
@@ -0,0 +1,20 @@
+package com.example.ariesgurl.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ariesgurl.R;
+import com.startapp.android.publish.adsCommon.StartAppAd;
+
+import package com.example.ariesgurl.R;
+
+public class BlankActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blank);
+//        StartAppAd.showAd(BlankActivity.this);
+    }
+}
